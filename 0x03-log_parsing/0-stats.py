9806ab7a4931f9b096e60821b62f6b7d246df0f9
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-07-17 11:49:54 +0000

    improved print logic

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,41 +14,41 @@ File size: 14525
 `
 """
 
-import sys
-from collections import defaultdict
+if __name__ == '__main__':
+    import sys
+    from collections import defaultdict
 
+    # Defines the global variables
+    total_file_size = 0
+    status_codes_dict = defaultdict(int)  # sets dict keys to int data type
+    status_codes_list = [200, 301, 400, 401, 403, 404, 405, 500]
+    total_line_cnt = 0
 
-# Defines the global variables
-total_file_size = 0
-status_codes_dict = defaultdict(int)  # sets dict keys to int data type
-status_codes_list = [200, 301, 400, 401, 403, 404, 405, 500]
-total_line_cnt = 0
+    try:
+        for line_count, line in enumerate(sys.stdin):
+            parts = line.split(" ")
+            status_code = int(parts[-2])
+            file_size = int(parts[-1])
 
-try:
-    for line_count, line in enumerate(sys.stdin):
-        parts = line.split(" ")
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
-
-        total_file_size += file_size
+            total_file_size += file_size
 
         # check if the status code is in the parsed status codes
-        if status_code in status_codes_list and type(status_code) == int:
-            # add the status code and set the count
-            status_codes_dict[status_code] += 1
+            if status_code in status_codes_list and type(status_code) == int:
+                # add the status code and set the count
+                status_codes_dict[status_code] += 1
 
-        if line_count % 10 == 0 and line_count > 0:
-            # for every 10 lines
-            print("File size: {}".format(total_file_size))
-            for status_code in sorted(status_codes_dict.keys()):
-                print("{}: {}".format(
+            if line_count % 10 == 0 and line_count > 0:
+                # for every 10 lines
+                print("File size: {}".format(total_file_size))
+                for status_code in sorted(status_codes_dict.keys()):
+                    print("{}: {}".format(
                                       status_code,
                                       status_codes_dict[status_code]
                                       ))
-    if line_count == 0:
+        if line_count == 0:
+            print("File size: {}".format(total_file_size))
+            print("{}: {}".format(status_code, status_codes_dict[status_code]))
+    except KeyboardInterrupt:  # Handle CTRL + C
         print("File size: {}".format(total_file_size))
-        print("{}: {}".format(status_code, status_codes_dict[status_code]))
-except KeyboardInterrupt:  # Handle CTRL + C
-    print("File size: {}".format(total_file_size))
-    for status_code in sorted(status_codes_dict.keys()):
-        print("{}: {}".format(status_code, status_codes_dict[status_code]))
+        for status_code in sorted(status_codes_dict.keys()):
+            print("{}: {}".format(status_code, status_codes_dict[status_code]))
