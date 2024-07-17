fad85af4c7c111114536155289213aad6c324146
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-07-17 11:45:17 +0000

    improved print logic

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,9 +22,10 @@ from collections import defaultdict
 total_file_size = 0
 status_codes_dict = defaultdict(int)  # sets dict keys to int data type
 status_codes_list = [200, 301, 400, 401, 403, 404, 405, 500]
+total_line_cnt = 0
 
 try:
-    for line_count, line in enumerate(sys.stdin, start=1):
+    for line_count, line in enumerate(sys.stdin):
         parts = line.split(" ")
         status_code = int(parts[-2])
         file_size = int(parts[-1])
@@ -36,7 +37,7 @@ try:
             # add the status code and set the count
             status_codes_dict[status_code] += 1
 
-        if line_count % 10 == 0:
+        if line_count % 10 == 0 and line_count > 0:
             # for every 10 lines
             print("File size: {}".format(total_file_size))
             for status_code in sorted(status_codes_dict.keys()):
@@ -44,7 +45,9 @@ try:
                                       status_code,
                                       status_codes_dict[status_code]
                                       ))
-
+    if line_count == 0:
+        print("File size: {}".format(total_file_size))
+        print("{}: {}".format(status_code, status_codes_dict[status_code]))
 except KeyboardInterrupt:  # Handle CTRL + C
     print("File size: {}".format(total_file_size))
     for status_code in sorted(status_codes_dict.keys()):
