5433dff1ec95037974beee5af5c41fa8330c5353
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-08-09 03:39:25 +0000

    imporoved doc

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-'''A script that defines a prime number'''
+'''A script that defines prime number operation and a game
+between Maria and Ben.'''
 
 
-def seive_of_eratosthenes(n):
-    """used to seive the prime numbers of n"""
+def sieve_of_eratosthenes(n):
+    """Uses the Sieve of Eratosthenes to fins all prime numbers up to n.
+    Args:
+        n (int) : The upper limit for finding prime numbers.
+    Returns:
+        list : A list of prime numbers up to n.
+        """
     # Create a boolean array
     # "prime[0..n]" and initialize all to true
     # A value in prime[i] will fina,y be false if i is not
@@ -24,39 +30,41 @@ def seive_of_eratosthenes(n):
 
 
 def isWinner(x, nums):
-    """
+    """Determines the winner between Maria and Ben based on the number of primes.
     Args:
-        nums (list): List of numbers
+        nums (list): List of numbers for each round
         x (int): The number of rounds
+    Returns:
+        str: The name of the Winner ('Maria' or 'Ben'), or None if it's a tie
     """
     marias_score = 0
     bens_score = 0
 
     for i in range(x):
-        if i > len(nums) - 1:
+        if i >= len(nums):
             break
         n = nums[i]
-        prime = seive_of_eratosthenes(n)
+        primes = sieve_of_eratosthenes(n)
 
-        if len(prime) == 0:
+        if len(primes) == 0:
             bens_score += 1
             continue
 
         maria_picks_count = 0
         ben_picks_count = 0
 
-        for j in prime:
+        for j in primes:
             if (i + 1) % 2 == 1:  # Marias turn
                 maria_picks_count += 1
             else:
                 ben_picks_count += 1  # Bens turn
         if maria_picks_count > ben_picks_count:
             marias_score += 1
-        if ben_picks_count > maria_picks_count:
+        elif ben_picks_count > maria_picks_count:
             bens_score += 1
 
     if marias_score > bens_score:
         return 'Maria'
-    if bens_score > marias_score:
+    elif bens_score > marias_score:
         return 'Ben'
     return None
